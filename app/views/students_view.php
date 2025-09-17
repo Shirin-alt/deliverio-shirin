f13d217eb8a163d12fe2610330654eed0c5f20fd
Shirin <deliverioshirin@gmail.com>
2025-09-18 00:13:55 +0800

    Add .htaccess for Apache mod_rewrite routing

--- app/views/students_view.php
+++ app/views/students_view.php
@@ -124,7 +124,7 @@
 <body>
     <h1>🎀 Student List 🎀</h1>
 
-    <form method="POST" action="/students/create">
+    <form method="POST" action="/students">
     <input type="text" name="last_name" placeholder="Last Name" required>
     <input type="text" name="first_name" placeholder="First Name" required>
     <input type="email" name="email" placeholder="Email" required>
